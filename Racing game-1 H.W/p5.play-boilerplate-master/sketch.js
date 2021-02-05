9918265df6f9c3ede2076d74380ff25e68691c5e
Karmanya256 <73025872+Karmanya256@users.noreply.github.com>
2021-02-05 08:25:15 +0530

    Add files via upload

---
+++ Racing game-1 H.W/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,43 @@
+var ballon;
+var database;
+var position;
+
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    ballon = createSprite(250,250,10,10);
+    ballon.shapeColor = "black";
+    var ballonPosition = database.ref("ballon/position")
+    ballonPosition.on("value",read)
+}
+
+function draw(){
+    background("green");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.ref("ballon/position").set({
+        x:position.x+x,y:position.y+y
+
+    });
+
+    
+}
+function read(data){
+    position=data.val();
+    ballon.x=position.x;
+    ballon.y=position.y;
+}
